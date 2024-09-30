1b8a3c4f14a7f03cdce13390e5a43c961fa450fb
Akos Fabian <fabian.akos@gmail.com>
2024-09-30 18:12:13 +0200

    starting loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import GHC.IO.Handle (BufferMode (NoBuffering), hSetBuffering)
+import GHC.IO.Handle.FD (stdin)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  hSetBuffering stdin NoBuffering
+  loop 1
+
+loop :: Int -> IO ()
+loop model = do
+  -- printf "Model is %d\n" model
+  view model
+  c <- getChar
+  newModel <- update c model
+  loop newModel
+
+-- main :: IO ()
+-- main = do
+--   putStrLn "Try typing and deleting!"
+--   s <- getLine
+--   printf "You typed %s\n" s
+--   putStrLn "Try presisng a key!"
+--   hSetBuffering stdin NoBuffering
+--   c <- getChar
+--   printf "You typed %c\n" c
+--   putStrLn "Done."
