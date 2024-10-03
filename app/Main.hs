30d87579923ee779b58dc222c0be4e0ca0e3b073
Akos Fabian <fabian.akos@gmail.com>
2024-10-03 19:38:09 +0200

    mark start stop

--- app/Main.hs
+++ app/Main.hs
@@ -137,6 +137,7 @@ update msg model =
         Key 'u' -> model {compareTick = tick model}
         Key ' ' -> model {tasks = markDone (tasks model) (index model)}
         Key 't' -> model {tasks = markTodo (tasks model) (index model)}
+        Key 's' -> model {tasks = markStartStop (tasks model) (index model)}
         KeyUnknown mode c -> model {logs = newLog : logs model}
           where
             newLog = "Unknown character " ++ show c ++ " in " ++ show mode ++ " mode."
@@ -157,6 +158,18 @@ markDone = markState Done
 
 markTodo :: V.Vector Task -> Maybe Int -> V.Vector Task
 markTodo = markState Todo
+
+markStartStop :: V.Vector Task -> Maybe Int -> V.Vector Task
+markStartStop ts Nothing = ts
+markStartStop ts (Just i) =
+  case (V.!?) ts i of
+    Nothing -> ts
+    (Just t) -> V.update ts (V.singleton (i, t {state = newState (state t)}))
+  where
+    newState Todo = Started
+    newState Started = Stopped
+    newState Stopped = Started
+    newState Done = Done -- explicit Todo state is needed via different key
 
 markState :: TaskState -> V.Vector Task -> Maybe Int -> V.Vector Task
 markState _ ts Nothing = ts
